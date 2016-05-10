c2e0cea758f7b9760515bd8f4cc3b649620999bb
apenchan <a.penchansky@gmail.com>
2016-05-10 14:05:34 -0400

    first comment and pushing my html for project

---
+++ 4.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	console.log("Here and working!");
+});
+
+// objectives by date:
+//5.10 Add onclick to each column
+//add color to each player. This includes defiing players
+// 
