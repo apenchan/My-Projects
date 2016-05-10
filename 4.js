9a97a8f4f2bc00519ab84f4e3c7e732859fa760d
apenchan <a.penchansky@gmail.com>
2016-05-10 15:00:56 -0400

    revlation on this element. added notes and psuedo code

--- 4.js
+++ 4.js
@@ -4,5 +4,32 @@ $(document).ready(function(){
 
 // objectives by date:
 //5.10 Add onclick to each column
-//add color to each player. This includes defiing players
-// 
+//5.10 add color to each player. This includes defining player1 and player2
+// 5.10 Each onclick must have a trickle down (in the "#game-tiles" div)
+// declare players
+
+var player1 = 1
+var player2 = 0
+
+//Onclick Objective
+// player can select a square. 
+//player can click occupied square, as long as row isn't occupied
+// each square must trickle to the bottom of the column
+//if row is occupied, then row can not be selected
+
+$(".column").click(function(){
+	console.log("You have clicked!");
+	var columnSelect = $(this);// this makes it so only the first item is clickable
+	if (columnSelect.hasClass("X") || columnSelect.hasClass("O")) {
+		alert("Select another column");//we have not yet added what the values to players
+		//will be using "X" and "O" to keep track of each player move (easier on the eyes)
+	} else {
+		if (player1 === 1){
+			columnSelect.addClass("X");
+			player1 +=1;
+		} else {
+			columnSelect.addClass("0");
+			player2 +=1;
+		}
+	}
+});
